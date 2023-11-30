fd2d4eddcc4e5c4c19212d24da3d97ecb84dc4ac
§ <mohgeis@gmail.com>
2023-11-30 19:39:19 +0100

    update cors sources

--- app/app/settings.py
+++ app/app/settings.py
@@ -18,11 +18,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
 SECRET_KEY_FALLBACKS = [
     os.environ.get('SECRET_KEY_FALLBACKS'),
 ]
@@ -48,17 +48,21 @@ ALLOWED_HOSTS = [
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.textshare.top",
+    "https://textshare.top",
 ]
 
+CORS_ALLOW_ALL_ORIGINS = False
 # For production (list of frontend's origin)
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://textshare.top",
-    "http://www.textshare.top",
     "https://textshare.top",
+    "http://www.textshare.top",
     "https://www.textshare.top",
+    "http://admin.textshare.top",
     "https://admin.textshare.top",
+    "http://www.admin.textshare.top",
     "https://www.admin.textshare.top",
 ]
 
@@ -110,7 +114,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -120,13 +123,12 @@ DATABASES = {
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
-        #'USER': b64decode(os.environ.get('DB_USER')).decode('utf-8'),
+        # 'USER': b64decode(os.environ.get('DB_USER')).decode('utf-8'),
         'PASSWORD': os.environ.get('DB_PASS'),
-        #'PASSWORD': b64decode(os.environ.get('DB_PASS')).decode('utf-8'),
+        # 'PASSWORD': b64decode(os.environ.get('DB_PASS')).decode('utf-8'),
 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -146,7 +148,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -159,7 +160,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
